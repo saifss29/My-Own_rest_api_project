0ce014740c97a8d62453f944bc412a256fc928b1
saifss29 <saifss2998@gmail.com>
2026-08-28 00:50:21 +0600

    html element mail

--- task.py
+++ task.py
@@ -1,9 +1,16 @@
 import requests
 import os
 from dotenv import load_dotenv
+import jinja2
 
 load_dotenv()
-def send_simple_message(to, subject, body):
+
+template_loader = jinja2.FileSystemLoader("templates")
+template_env = jinja2.Environment(loader=template_loader)
+
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
+def send_simple_message(to, subject, body, html):
     domain = os.getenv("MAILGUN_DOMAIN")
 
     return requests.post(
@@ -21,5 +28,6 @@ def send_user_registration_email(email, username):
     return send_simple_message(
         email,
         "Successfully signed up",
-        f"Hi{username}! You have successfully signed up to the Stores REST API."
+        f"Hi {username}! You have successfully signed up to the Stores REST API.",
+        render_template("email/registration.html", username=username),
     )
